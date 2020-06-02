da082e1118a1f85cac53f228caed170bbb4d6a65
Juhae Lee <juhae.lee@gmail.com>
2020-06-02 15:02:49 +0300

    DeleteSubCategory

--- Areas/Admin/Controllers/SubCategoryController.cs
+++ Areas/Admin/Controllers/SubCategoryController.cs
@@ -27,7 +27,7 @@ namespace Mosdong.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             var subCategories = await _db.SubCategory.Include(s => s.Category).ToListAsync();
-            return View(subCategories);
+            return View(subCategories.OrderBy(p => p.Name).ToList());
         }
 
         //GET - Create
@@ -79,9 +79,111 @@ namespace Mosdong.Areas.Admin.Controllers
         {
             List<SubCategory> subCategories = new List<SubCategory>();
             subCategories = await (from subCategory in _db.SubCategory
+                                   orderby subCategory.Name
                                    where subCategory.CategoryId == id
                                    select subCategory).ToListAsync();
-            return Json(new SelectList(subCategories, "id", "Name"));
+            return Json(new SelectList(subCategories, "Id", "Name"));
+        }
+
+        //GET - Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var subCategory = await _db.SubCategory.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+            SubCategoryAndCategoryViewModel model = new SubCategoryAndCategoryViewModel
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = subCategory,
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).Distinct().ToListAsync()
+            };
+
+            return View(model);
+        }
+
+        //POST - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(SubCategoryAndCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var doesSubCategoryExists = _db.SubCategory.Include(s => s.Category).Where(s => s.Name == model.SubCategory.Name && s.Category.Id == model.SubCategory.CategoryId);
+                if (doesSubCategoryExists.Count() > 0)
+                {
+                    //Error
+                    StatusMessage = "에러: " + doesSubCategoryExists.First().Category.Name + "카테고리 내에 이미 존재하는 항목입니다. 새로운 항목을 입력하세요.";
+
+                }
+                else
+                {
+                    var subCatFromDb = await _db.SubCategory.FindAsync(model.SubCategory.Id);
+                    subCatFromDb.Name = model.SubCategory.Name;
+
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            SubCategoryAndCategoryViewModel modelVM = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = model.SubCategory,
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).ToListAsync(),
+                StatusMessage = StatusMessage
+            };
+            return View(modelVM);
+        }
+
+        //GET Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var subCategory = await _db.SubCategory.Include(s => s.Category).SingleOrDefaultAsync(m => m.Id == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subCategory);
+        }
+
+        //GET - Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var subCategory = await _db.SubCategory.Include(s => s.Category).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subCategory);
+        }
+
+        //POST - Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var subCategory = await _db.SubCategory.SingleOrDefaultAsync(m => m.Id == id);
+            _db.SubCategory.Remove(subCategory);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
---
+++ Models/ViewModels/CategoryNavigationModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mosdong.Models
+{
+    public class CategoryNavigationModel 
+    {
+    public List<Category> CategoryList { get; set; }
+    public SubCategory SubCategory { get; set; }
+    public List<string> CurrentSubCategoryList { get; set; }
+    public int CurrentCategoryId { get; set; }
+
+
+        #region Nested classes
+
+        public class CategoryLineModel 
+    {
+        public int CurrentCategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+
+        #endregion
+    }
+}
